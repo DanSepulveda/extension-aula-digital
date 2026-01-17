4e00fed809f6ff166c4074e8f8e598d61523709b
Daniel Sepúlveda <dansepdev@gmail.com>
2026-01-17 10:17:25 -0300

    Add content script and icons for Aula SENCE extension

---
+++ src/content.js
@@ -0,0 +1,54 @@
+const sections = [
+  'General',
+  'Aula Sincrónica',
+  'Orientación al perfil y metodología del curso/ELE071067098636',
+  'Fundamentos de desarrollo Front-End/ELE071067098637',
+  'Fundamentos de programación en Python/ELE071067098638',
+  'Programación avanzada en Python/ELE071067098639',
+  'Fundamentos de bases de datos/ELE07106709relacionales 8640',
+  'Desarrollo de aplicaciones web con Python Django/ELE071067098641',
+  'Acceso a datos en aplicaciones Python Django/ELE071067098642',
+  'Desarrollo de portafolio de un producto digital/ELE071067098643',
+  'Desarrollo de empleabilidad en la industria digital/ELE071067098644',
+];
+
+const originalTitles = sections.map((_, index) => {
+  const id = `section-${index}`;
+  const el = document.getElementById(id);
+  const link = el.querySelector('h3 a');
+  return link.textContent;
+});
+
+const apply = () => {
+  chrome.storage.sync.get(null, (config) => {
+    const fixTitles = config['fix-titles'];
+
+    sections.forEach((section, index) => {
+      const id = `section-${index}`;
+      const el = document.getElementById(id);
+      if (!el) return;
+
+      const link = el.querySelector('h3 a');
+
+      fixTitles
+        ? (link.textContent = section)
+        : (link.textContent = originalTitles[index]);
+
+      const visible = config[id] ?? true;
+      el.style.display = visible ? '' : 'none';
+    });
+  });
+};
+
+// on page load
+apply();
+
+// on checkbox change
+chrome.storage.onChanged.addListener(apply);
+
+// in case of dynamic page
+const observer = new MutationObserver(apply);
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
